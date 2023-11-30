8ad0952e373b4d762e5de6913912f19867b06c2f
yvzklc <kilciyavuz34@gmail.com>
2023-11-30 15:25:01 +0300

    finished

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,12 @@
-import React from 'react'
 import './Login.css'
 export const Login = () => {
   return (
-   
-     
+    <div>
+    <div className="container">
+    <input type="Email" placeholder='Email'></input>
+    <input type="text" placeholder='Password'></input>
+    <button>Log in</button>
+    </div>
+    </div>
   )
 }
